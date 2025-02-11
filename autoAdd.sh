e1f190d00df5f43cc42da09fc9b4aa588558b408
HeadedBranch <isaac.hesslegrave@gmail.com>
2025-02-11 23:04:09 +0000

    actually final this time hopefully

--- autoAdd.sh
+++ autoAdd.sh
@@ -8,7 +8,7 @@ elif [ $# -eq 1 ]; then
 	echo "$(date '+%Y%m%d') $1" >> README.md
 else
 	echo "enter movie name"
-	read choice
+	read -r choice
 	echo "\"$(date '+%Y%m%d')\",\"$choice\"" >> list.csv
 	echo "$(date '+%Y%m%d') $choice" >> README.md
 fi
