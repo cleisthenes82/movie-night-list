5bbebb0544ace33f040aedc7cd44d7b36f8db9d7
HeadedBranch <isaac.hesslegrave@gmail.com>
2025-02-11 23:02:39 +0000

    fix year format

--- autoAdd.sh
+++ autoAdd.sh
@@ -4,12 +4,12 @@ if [ $# -eq 2 ]; then
 	echo "\"$1\",\"$2\"" >> list.csv
 	echo "$1 $2" >> README.md
 elif [ $# -eq 1 ]; then
-	echo "\"$(date '+%y%m%d')\",\"$1\"" >> list.csv
-	echo "$(date '+%y%m%d') $1" >> README.md
+	echo "\"$(date '+%Y%m%d')\",\"$1\"" >> list.csv
+	echo "$(date '+%Y%m%d') $1" >> README.md
 else
 	echo "enter movie name"
 	read choice
-	echo "\"$(date '+%y%m%d')\",\"$choice\"" >> list.csv
-	echo "$(date '+%y%m%d') $choice" >> README.md
+	echo "\"$(date '+%Y%m%d')\",\"$choice\"" >> list.csv
+	echo "$(date '+%Y%m%d') $choice" >> README.md
 fi
 
