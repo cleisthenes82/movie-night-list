b48f0bd44052f3b5bd1928c6ee0440402f8f5545
HeadedBranch <isaac.hesslegrave@gmail.com>
2025-02-11 22:31:51 +0000

    add to script

--- autoAdd.sh
+++ autoAdd.sh
@@ -2,7 +2,9 @@
 
 if [ $# -eq 2 ]; then
 	echo "\"$1\",\"$2\"" >> list.csv
+	echo "$1 $2" >> README.md
 fi
 if [ $# -eq 1 ]; then
 	echo "\"$(date '+%y%m%d')\",\"$1\"" >> list.csv
+	echo "$(date '+%y%m%d') $1" >> README.md
 fi
