76ca0956d688be7dbf62a9a1978e7c7227b114ad
HeadedBranch <isaac.hesslegrave@gmail.com>
2025-02-21 16:46:57 +0000

    make the script update read me

--- autoAdd.sh
+++ autoAdd.sh
@@ -2,14 +2,14 @@
 
 if [ $# -eq 2 ]; then
 	echo "\"$1\",\"$2\"" >> list.csv
-	echo "$1 $2" >> README.md
+	echo "$1 $2 \\" >> README.md
 elif [ $# -eq 1 ]; then
 	echo "\"$(date '+%Y%m%d')\",\"$1\"" >> list.csv
-	echo "$(date '+%Y%m%d') $1" >> README.md
+	echo "$(date '+%Y%m%d') $1 \\" >> README.md
 else
 	echo "enter movie name"
 	read -r choice
 	echo "\"$(date '+%Y%m%d')\",\"$choice\"" >> list.csv
-	echo "$(date '+%Y%m%d') $choice" >> README.md
+	echo "$(date '+%Y%m%d') $choice \\" >> README.md
 fi
 
